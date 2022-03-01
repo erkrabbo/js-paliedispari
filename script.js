712d31bcce88529422a026c30fd2e7fb49ecca06
Luca Andaloro <luca.andaloro4@gmail.com>
2022-03-01 19:16:22 +0100

    da sistemare scelta pari/dispari (sempre pari)

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const msg = document.createElement('h1');
+let areThereElements = false;
+
 reset();
 document.body.prepend(msg);
+
 
 let palEx = document.querySelector('#palibtn');
 let parDisEx = document.querySelector('#evenOdd');
 let res = document.querySelector('#reset');
+
 
 palEx.addEventListener('click', paliExercise);
 parDisEx.addEventListener('click', evenOddExercise);
@@ -60,32 +64,92 @@ function paliExercise(){
 }
 
 function evenOddExercise(){
-    let choice = parseInt(prompt('Digita 0 per pari 1 per dispari:'));
+    const parDisChoice = document.createElement('select');
+    const parDisOption = document.createElement('option');
+    const arrChoice = ['pari', 'dispari'];
 
-    while (choice != 0 && choice != 1){
-        choice = parseInt(prompt('input errato. Digita 0 per pari 1 per dispari:'));
-        console.log(choice)
+    for (i = 0; i < 2; i++){
+        const parDisOption = document.createElement('option');
+        parDisOption.innerHTML = arrChoice[i];
+        parDisOption.value = 1;
+        if(parDisOption.innerHTML == 'pari'){
+            parDisOption.setAttribute('selected', 'selected');
+            parDisOption.value = 0;
+        }
+        parDisChoice.append(parDisOption);
     }
 
-    let userNumber = parseInt(prompt('Scegli un numero da 1 a 5: '));
+    msg.innerHTML = 'Scegli pari o dispari, inserisci nell\'apposito campo il tuo numero(compreso tra 1 e 5) e poi clicca su gioca.';
+    document.body.append(parDisChoice);
+    
+    let inputNumber = document.createElement('input');
+    inputNumber.setAttribute('type', 'number');
+    inputNumber.setAttribute('placeholder', 'inserisci qui il tuo numero');
+    document.body.append(inputNumber);
+    
+    let gioca = document.createElement('button');
+    gioca.innerHTML = 'GIOCA';
+    gioca.setAttribute('id', 'gioca');
+    document.body.append(gioca);
+    gioca.addEventListener('click',play)
 
-    while (userNumber < 1 || userNumber > 5 || isNaN(userNumber)){
-        userNumber = parseInt(prompt('Non hai inserito un numero valido. Scegli un numero da 1 a 5: '));
-    }
+    areThereElements = true;
 
-    let pcNumber = Math.floor(Math.random() * 5 ) + 1;
-
-    let sum = userNumber + pcNumber;
-
-    if ((sum % 2 && choice) || ( !(sum % 2 && !choice))){
-        console.log(pcNumber);
-        msg.innerHTML = `il computer ha scelto ${pcNumber}. La somma è ${sum}. Grande hai vinto!` ;
-    } else{
-        console.log(pcNumber);
-        msg.innerHTML = `il computer ha scelto ${pcNumber}. La somma è ${sum}. Ritenta sarai più fortunato...`
+    function play(){
+        let userNumber = parseInt(inputNumber.value);
+        let pcNumber = 0;
+        let sum = 0;
+    
+        if (userNumber < 1 || userNumber > 5 || isNaN(userNumber)){
+            alert('Inserisci un numero corretto')
+            document.body.removeChild(gioca);
+            document.body.removeChild(inputNumber);
+            document.body.removeChild(parDisChoice);
+            evenOddExercise();
+        } else {
+            pcNumber = Math.floor(Math.random() * 5 ) + 1;
+    
+            sum = userNumber + pcNumber;
+    
+            const selIndex = parDisChoice.selectedIndex;
+            const choice = parDisChoice.options[selIndex];
+    
+            if ((sum % 2 && choice) || ( !(sum % 2 && !choice))){
+                console.log(pcNumber);
+                msg.innerHTML = `il computer ha scelto ${pcNumber}. La somma è ${sum}. Grande hai vinto!` ;
+            } else{
+                console.log(pcNumber);
+                msg.innerHTML = `il computer ha scelto ${pcNumber}. La somma è ${sum}. Ritenta sarai più fortunato...`
+            }
+        }
     }
 }
 
 function reset(){
     msg.innerHTML = 'SCEGLI QUALE ESERCIZIO CORREGGERE';
+
+    if (areThereElements){
+        const gioca = document.body.querySelector('#gioca');
+        const inputNumber = document.body.querySelector('input');
+        const parDisChoice = document.body.querySelector('select');
+
+        document.body.removeChild(gioca);
+        document.body.removeChild(inputNumber);
+        document.body.removeChild(parDisChoice);
+
+        areThereElements = false;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
